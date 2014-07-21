aee8ae28873dff11fd91df29bb24148101ed3684
bryan kennedy <bkennedy@smm.org>
2014-07-21 16:48:20 -0500

    Serial setup for ou devices

--- control.py
+++ control.py
@@ -18,3 +18,15 @@ Options:
 from docopt import docopt
 import time
 import serial
+
+# Configure the serial connection
+ser = serial.Serial(
+    # We're using a KeySpan USB to Serial Adapter and the provided driver
+    # This will be different for different adapters and drivers
+    port='/dev/cu.KeySerial1',
+    # Canon projector serial communication specifications
+    baudrate=19200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_TWO,
+    bytesize=serial.EIGHTBITS
+)
