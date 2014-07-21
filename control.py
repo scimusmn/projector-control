3b826c10de8e0b51dc12528ef043a12c67be0d64
bryan kennedy <bkennedy@smm.org>
2014-07-21 16:54:14 -0500

    Ask the projector for the current power state
    We can't ask it to turn on if it's already on. We also should detect for
    transitional power states.

--- control.py
+++ control.py
@@ -19,6 +19,9 @@ from docopt import docopt
 import time
 import serial
 
+# Seconds to wait for serial responses
+RESPONSEWAIT = 1
+
 # Configure the serial connection
 ser = serial.Serial(
     # We're using a KeySpan USB to Serial Adapter and the provided driver
@@ -30,3 +33,29 @@ ser = serial.Serial(
     stopbits=serial.STOPBITS_TWO,
     bytesize=serial.EIGHTBITS
 )
+
+def get_power_state():
+    """Get the projector's current power state
+
+    Returns:
+        A string containing the projector control code for the current
+        power state.
+
+        'g:POWER=OFF'    = OFF
+        'g:POWER=OFF2ON' = OFF -> ON in transition
+        'g:POWER=ON'     = ON
+        'g:POWER=ON2PMM' = ON -> Standby in transition
+        'g:POWER=PMM'    = Standby
+        'g:POWER=PMM2ON' = Standby -> ON in transition
+        'g:POWER=ON2OFF' = ON-> OFF in transition
+    """
+    print "Getting current power state"
+    ser.write('GET POWER' + '\r\n')
+    response = ''
+    time.sleep(RESPONSEWAIT)
+    while ser.inWaiting() > 0:
+        response += ser.read(1)
+    print '\tProjector power state: ' + response
+    response = response.replace('\n', '').replace('\r', '')
+    return response
+
