518dde74163d7e800c38494f72a589f92cf01338
bryan kennedy <bkennedy@smm.org>
2014-07-21 16:45:57 -0500

    Initial options for docopt
    This handles the command line arguments

--- control.py
+++ control.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python
+"""Simple projector control script
+
+This is written to control power on a Canon SX6000 projector
+
+Usage:
+  control.py power (--off | --on)
+  control.py (-h | --help)
+  control.py --version
+
+Options:
+  power --on         Power on
+  power --off        Power off
+  -h --help          Show this screen.
+  --version          Show version
+
+"""
+from docopt import docopt
